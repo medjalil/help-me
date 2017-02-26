7b2017f48d6cca5e50f4425d214d1910c245609e
Mohamed ABDELJALIL <med.abdeljalil@gmail.com>
2017-02-26 11:39:25 +0100

    création des pages de site

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,16 +4,36 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller {
 
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="home")
      */
-    public function indexAction(Request $request) {
-        // replace this example code with whatever you need
+    public function indexAction() {
         return $this->render('default/index.html.twig');
+    }
+
+    /**
+     * @Route("/about", name="about")
+     */
+    public function aboutAction() {
+        return $this->render('default/about.html.twig');
+    }
+
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function blogAction() {
+        return $this->render('default/blog.html.twig');
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contactAction() {
+
+        return $this->render('default/contact.html.twig');
     }
 
 }
